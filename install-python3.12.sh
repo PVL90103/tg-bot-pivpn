67f1481285d1a85e3913a43cd9c54fb19ce58a7d
Rumble <rumble@deeplay.io>
2024-12-09 18:57:14 +0600

    add makefile + sh

---
+++ install-python3.12.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install -y python3.12 python3.12-venv make
+python3.12 --version
+make --version
