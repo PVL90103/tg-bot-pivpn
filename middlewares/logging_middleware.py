4cb0879f06e67dfd4fc25325612ab0364edcf6b0
Rumble <rumble@deeplay.io>
2024-12-16 14:37:03 +0600

    fix logger

--- middlewares/logging_middleware.py
+++ middlewares/logging_middleware.py
@@ -1,13 +1,29 @@
 import logging
+from logging.handlers import TimedRotatingFileHandler
+import os
+import time
 from aiogram import BaseMiddleware
 from aiogram.types import Update
 
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+file_handler = TimedRotatingFileHandler(
+    filename=os.path.join(LOG_DIR, "bot.log"),
+    when="midnight",
+    interval=1,
+    backupCount=7
+)
+
+file_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
     handlers=[
-        logging.FileHandler("bot.log"),  # Запись логов в файл
-        logging.StreamHandler()          # Вывод логов в консоль
+        file_handler,                 # Запись логов в файл
+        logging.StreamHandler()       # Вывод логов в консоль
+
     ]
 )
 logger = logging.getLogger(__name__)
